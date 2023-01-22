2c892a874ca15214dd5630723d468b43903d0cce
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-01-22 21:00:46 +0900

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p className='text-red-500 text-3xl font-bold underline'>hello world!</p>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'sp': '0px',
+      'desktop': '897px'
+  },
+  },
+  plugins: [],
+}
