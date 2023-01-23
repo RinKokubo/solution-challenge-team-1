820bc8fab585bc11ce6b2f24a82fcdce2422aa34
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-01-23 16:48:20 +0900

    add google map api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <LoadScript googleMapsApiKey="AIzaSyCeNQPZSNTDWlBeTcZwsjjuBPYa2hBURMM">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}>
+          { /* Child components, such as markers, info windows, etc. */ }
+          <></>
+      </GoogleMap>
+    </LoadScript>
   )
 }
 
