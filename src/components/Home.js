a05830416ca876b4d76e07f8b05d0951b97c1a76
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-02-06 16:30:39 +0900

    fix width

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: '320px',
+  height: '60vh'
 };
 
 const center = {
