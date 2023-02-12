a2cf5179509160a8f52507a7bab4a666e422c7f1
RinKokubo <86641034+RinKokubo@users.noreply.github.com>
2023-02-12 20:42:00 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
-import Home from './components/Sign_in';
-import Home from './components/Confirm';
+import Sign_in from './components/Sign_in';
+import Confirm from './components/Confirm';
 
 function App() {
   return (
