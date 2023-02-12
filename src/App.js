84c55ef52f13255a1e6b7484ae22353d10561138
RinKokubo <86641034+RinKokubo@users.noreply.github.com>
2023-02-12 20:04:45 +0900

    Update App.js
    コンポーネントをファイルの中で使用するには、まずはimportしてあげて！

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
+import Home from './components/Sign_in';
+import Home from './components/Confirm';
 
 function App() {
   return (
