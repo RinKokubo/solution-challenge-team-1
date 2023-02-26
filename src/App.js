20bf4c490898095c8da02d7be6e539fb18535ff3
kamechan8 <56075596+kamechan8@users.noreply.github.com>
2023-02-26 17:01:53 +0900

    add sign in function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Map from './components/Map';
 import CreateAccount from './components/CreateAccount';
 import Confirm from './components/Confirm';
 import Start from './components/Start';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Start />}></Route>
         <Route path='/map' element={<Map />}></Route>
-        <Route path='/sign_in' element={<CreateAccount />}></Route>
-        <Route path='/sign_in/confirm' element={<Confirm />}></Route>
+        <Route path='/sign_in' element={<SignIn />}></Route>
+        <Route path='/sign_up' element={<CreateAccount />}></Route>
+        <Route path='/sign_up/confirm' element={<Confirm />}></Route>
       </Routes>
     </Router>
   );
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,17 +1,39 @@
 import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.css';
+import { auth } from '../firebase';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+    console.log(email.value, password.value);
+    signInWithEmailAndPassword(auth,email.value, password.value)
+      .then((userCredential) => {
+        navigate('/map');
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        alert("メールアドレスかパスワードが間違っています。");
+      });
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control name= "email" type="email" placeholder="Enter email" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control name= "password" type="password" placeholder="Password" />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
