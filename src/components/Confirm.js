30c01d91a888e3f4a47e4a8f9f49cdf9ac45f14b
kamechan8 <56075596+kamechan8@users.noreply.github.com>
2023-03-07 20:24:02 +0900

    create account

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -1,4 +1,32 @@
+import React, {useEffect, useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.css';
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { useLocation } from 'react-router-dom';
+
 const Confirm = () => {
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { email,password } = location.state;
+  console.log(email, password);
+
+  useEffect(() => {
+    createUserWithEmailAndPassword(auth,email, password)
+      .then((userCredential) => {
+        navigate('/sign_in');
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  }, []);
+
   return (
     <p>hello</p>
   )
--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,7 +1,59 @@
-const SignIn = () => {
+import React, {useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.css';
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+const CreateAccount = () => {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+    console.log(email.value, password.value);
+    navigate('/sign_up/confirm', {state: {email: email.value, password: password.value}});
+  };
+
   return (
-    <p>hi</p>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control name= "name" type="text" placeholder="" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control name= "email" type="email" placeholder="" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control name= "password" type="password" placeholder="" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formAge">
+        <Form.Label>Age</Form.Label>
+        <Form.Control name= "age" type="text" placeholder="" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formSex">
+        <Form.Label>Sex</Form.Label>
+        <DropdownButton id="sex" title="Sex">
+          <Dropdown.Item href="#/action-1">Male</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Female</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Ohter</Dropdown.Item>
+        </DropdownButton>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formCountry">
+        <Form.Label>Country</Form.Label>
+        <Form.Control name= "country" type="text" placeholder="" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   )
 }
 
-export default SignIn
+export default CreateAccount
